16d07f7ee189232d218663c1926e3cfc67c2e0c0
wanirepo <choongwan.woo@gmail.com>
2019-04-30 16:57:39 +0900

    update

---
+++ activities/L14_central_limit_theorem_bimodal.m
@@ -0,0 +1,25 @@
+%% Simulate the central limit theorem with bimodal data
+%%
+% data generation
+a = [normrnd(10,3,1000,1);normrnd(20,2,1000,1)];
+histogram(a);
+%% random sampling
+%%
+
+% random sampling n = 10, 30, 60, 100
+
+sample_n = [10 30 60 100];
+for iter = 1:10000
+    for i = 1:numel(sample_n)
+        m{i}(iter,1) = mean(a(randperm(numel(a),i)));
+    end
+end
+
+%% figure
+%%
+for i = 1:numel(m)
+    subplot(2,2,i);
+    histogram(m{i});
+    set(gca, 'xlim', [0 25]);
+    title(['sample n = ' num2str(sample_n(i))]);
+end
